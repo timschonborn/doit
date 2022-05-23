4d7e4e5f36762305df2f4710208623f1b851cf95
Tim Schoenborn <t.j.schonborn@gmail.com>
2022-05-23 21:15:29 +0200

    updated design based on tailwind tutorial

--- web/src/components/Item/Item.stories.tsx
+++ web/src/components/Item/Item.stories.tsx
@@ -1,7 +1,12 @@
 import Item from './Item'
 
 export const generated = ({
-  item = { id: '1', title: 'Test title', body: '', createdAt: Date.now() },
+  item = {
+    id: '1',
+    title: 'Test title',
+    body: `I'm baby wayfarers everyday carry tote bag cornhole master cleanse air plant lomo fam beard XOXO dreamcatcher deep v. Hot chicken twee af listicle single-origin coffee brunch cliche hammock street art etsy drinking vinegar gastropub migas VHS offal. Fam kickstarter cardigan four loko glossier authentic banh mi kale chips raw denim vice cred tonx. Ramps etsy bushwick fashion axe, pinterest you probably haven't heard of them vinyl sustainable drinking vinegar put a bird on it flannel.`,
+    createdAt: Date.now(),
+  },
 }) => {
   return <Item item={item} />
 }
--- web/src/components/Item/Item.tsx
+++ web/src/components/Item/Item.tsx
@@ -2,14 +2,22 @@ import { Link, routes } from '@redwoodjs/router'
 
 const Item = ({ item }) => {
   return (
-    <article>
+    <article className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 ring-slate-900/5">
       <header>
-        <Link to={routes.item({ id: item.id })}>
+        <Link
+          className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight"
+          to={routes.item({ id: item.id })}
+        >
           <h2>{item.title}</h2>
         </Link>
       </header>
-      <p>{item.body}</p>
-      <small>{item.createdAt}</small>
+
+      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+        {item.body}
+      </p>
+      <small className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+        {new Date(item.createdAt).toLocaleString()}
+      </small>
     </article>
   )
 }
